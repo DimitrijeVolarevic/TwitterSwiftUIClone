598a091eb1fe801873f1c3e296a5ac0d55815e2c
DimitrijeVolarevic <dimitrijevolarevic9@gmail.com>
2023-04-24 19:32:00 +0200

    Initial Commit

---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Dimitrije Volarevic on 24.4.23..
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
