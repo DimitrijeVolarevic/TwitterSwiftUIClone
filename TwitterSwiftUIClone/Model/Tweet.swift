3b6fc56c4e210d755fbe7a6187af5c06aa46ddba
DimitrijeVolarevic <dimitrijevolarevic9@gmail.com>
2023-04-29 20:44:25 +0200

    Forgotten commit

---
+++ TwitterSwiftUIClone/Model/Tweet.swift
@@ -0,0 +1,22 @@
+//
+//  Tweet.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Dimitrije Volarevic on 29.4.23..
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+import Firebase
+
+struct Tweet: Identifiable, Codable {
+    
+    @DocumentID var id: String?
+    let caption: String
+    let timestamp: Timestamp
+    let uid: String
+    var likes: Int
+    
+    var user: User?
+    
+}
