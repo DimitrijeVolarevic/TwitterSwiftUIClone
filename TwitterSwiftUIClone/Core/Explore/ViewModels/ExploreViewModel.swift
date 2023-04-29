ff07397d3b246c2f2df5155178fcc98fe7cee4d8
DimitrijeVolarevic <dimitrijevolarevic9@gmail.com>
2023-04-29 19:08:22 +0200

    Search bar & Filter users added

--- TwitterSwiftUIClone/Core/Explore/ViewModels/ExploreViewModel.swift
+++ TwitterSwiftUIClone/Core/Explore/ViewModels/ExploreViewModel.swift
@@ -10,6 +10,19 @@ import Foundation
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
     let service = UserService()
+    @Published var searchText = "" 
+    
+    var searchableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercasedQuery = searchText.lowercased()
+            
+            return users.filter({
+                $0.username.contains(lowercasedQuery) || $0.fullname.lowercased().contains(lowercasedQuery)
+            })
+        }
+    }
     
     init() {
         fetchUsers()
